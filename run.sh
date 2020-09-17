7937e3b0018f596939a2b6ae1cb787961a7d5d22
uknbr <ukn_linux@yahoo.com.br>
2020-09-17 10:42:59 -0300

    Sync to MySQL

--- run.sh
+++ run.sh
@@ -4,10 +4,17 @@ BASEDIR=$(dirname "${BASH_SOURCE}")
 
 wait_time=${DAEMON_INTERVAL:-15}
 wait_time=$((wait_time * 60))
+db_sync=${MYSQL_SYNC:-False}
 
 while true ; do
 	echo "[$(date +'%D %T')] Running in silent mode with parameters: ${CAR_MODEL} | ${CAR_REGION} | ${CAR_DATE_BEGIN} | ${CAR_DATE_END}"
 	python3 ${BASEDIR}/car.py -r ${CAR_REGION} -b ${CAR_DATE_BEGIN} -e ${CAR_DATE_END} -q || true
+
+	if [ "${db_sync}" == "True" ] ; then
+		echo "[$(date +'%D %T')] Sync local data to MySQL (${MYSQL_HOST}:${MYSQL_PORT})"
+		sqlite3mysql -f olx/db/car.db -d ${MYSQL_DATABASE} -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u ${MYSQL_USER} -p ${MYSQL_PASS} -l olx/log/sync.log
+	fi
+
 	echo "[$(date +'%D %T')] Waiting ${wait_time} seconds for next execution"
 	sleep ${wait_time}
 done
